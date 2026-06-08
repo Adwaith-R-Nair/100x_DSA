5b089d441631c2e32c46affe5ea30bcdf810aff9
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-09 00:34:01 +0530

    prime and factos

---
+++ Day-1/Assignment-1/am_i_prime.py
@@ -0,0 +1,16 @@
+n = int(input())
+count = 0
+
+for i in range(1, int((n ** 0.5)) + 1):
+    if (n % i) == 0:
+        a = i
+        b = n / i
+        if a == b:
+            count += 1
+        else:
+            count += 2
+
+if count == 2:
+    print("YES")
+else:
+    print("NO")
