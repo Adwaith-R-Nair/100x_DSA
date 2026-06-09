d376b821eb70f8f1f0b24b044fa91614711fc7aa
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-10 00:00:14 +0530

    k factors

---
+++ Day-2/Assignment/k_factors.py
@@ -0,0 +1,28 @@
+def div(n):
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    return count
+
+
+n, q ,k = map(int, input().split())
+a = list(map(int, input().split()))
+
+p = []
+sum = 0
+
+for i in range(0, n):
+    if (div(a[i]) == k):
+        sum += 1
+    p.append(sum)
+
+for i in range(q):
+    l, r = map(int, input().split())
+    l -= 1
+    r -= 1
+
+    if (l == 0):
+        print(p[r])
+    else:
+        print(p[r] - p[l - 1])
