235749d502133414213573895d1fcf570fa06b93
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-12 00:00:08 +0530

    intersection

---
+++ Day-4/Assignment/intersection_of_two_arrays.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+s = set()
+ans = set()
+
+for i in a:
+    s.add(i)
+
+for i in b:
+    if i in s:
+        ans.add(i)
+
+if len(ans) > 0:
+    print(len(ans))
+    print(" ".join(map(str, ans)))
+else:
+    print(0)
+    print(ans)
