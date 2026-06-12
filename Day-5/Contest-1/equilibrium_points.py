9a929d8591edff0aa444dcbe8ed6ca0d5e96a3b1
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-12 12:07:58 +0530

    vowel consonant

--- Day-5/Contest-1/equilibrium_points.py
+++ Day-5/Contest-1/equilibrium_points.py
@@ -11,9 +11,9 @@ total = p[-1]
 count = 0
 
 for i in range(n):
-    left  = p[i]
-    right = total - (p[i - 1] if i > 0 else 0)
-    if left == right:
+    l  = p[i]
+    r = total - (p[i - 1] if i > 0 else 0)
+    if l == r:
         count += 1
 
 print(count)
