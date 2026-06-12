374bc322ed7cfd834c57ceebf5ac194781320fe5
Adwaith R Nair <adwaith.r.nair189@gmail.com>
2026-06-12 13:29:12 +0530

    modified i to j

--- Day-05/Contest-1/good_pairs.py
+++ Day-05/Contest-1/good_pairs.py
@@ -4,17 +4,17 @@ a = list(map(int, input().split()))
 mp = {}
 count = 0
 
-for i in range(n):
+for j in range(n):
     if k == 0:
-        if a[i] == 0:
-            count += i
-    elif a[i] % k == 0:
-        req = a[i] // k
+        if a[j] == 0:
+            count += j
+    elif a[j] % k == 0:
+        req = a[j] // k
         if req in mp:
             count += mp[req]
-    if a[i] in mp:
-        mp[a[i]] += 1
+    if a[j] in mp:
+        mp[a[j]] += 1
     else:
-        mp[a[i]] = 1
+        mp[a[j]] = 1
 
 print(count)
