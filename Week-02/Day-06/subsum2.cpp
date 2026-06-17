c592c5cf6cf9eeabe8993c4dbc0e559cf137ce3a
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-06-18 01:16:11 +0530

    subsum

---
+++ Week-02/Day-06/subsum2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += a[i] * (i + 1) * (n - i);
+    }
+    cout << ans;
+}
