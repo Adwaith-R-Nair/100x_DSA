c7d0e971e2b9b3a9759249a9df2aa5add3cf4590
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-06-18 15:28:22 +0530

    max

---
+++ Week-02/Day-07/max.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<long long> a(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    long long sum = 0;
+
+    for(int i = 0; i < k; i++){
+        sum += a[i];
+    }
+    long long ans = sum;
+
+    for(int i = k; i < n; i++){
+        sum += a[i];
+        sum -= a[i - k];
+        ans = max(ans, sum);
+    }
+    cout << ans;
+
+}
