c9c1764d030965e452f7192033bbc0a64e53f727
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-06-19 11:45:12 +0530

    intern

---
+++ Week-02/Day-10/Contest-2/intern.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int l = 0; l < n; l++){
+        int mx = a[l];
+        for(int r = l; r < n; r++){
+            mx = max(mx, a[r]);
+            cout << mx << " ";
+        }
+    }
+
+}
