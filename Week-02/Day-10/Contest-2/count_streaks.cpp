50564c0f6271a7b9bc07aa2a96557af45b9b9c9a
Adwaith-R-Nair <adwaith.r.nair189@gmail.com>
2026-06-19 12:33:13 +0530

    count streak

---
+++ Week-02/Day-10/Contest-2/count_streaks.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    unordered_set<int> window;
+    int l = 0;
+    long long ans = 0;
+
+    for(int r = 0; r < n; r++){
+        while(window.count(a[r])){
+            window.erase(a[l]);
+            l++;
+        }
+        window.insert(a[r]);
+        ans += r - l + 1;
+    }
+
+    cout << ans;
+}
